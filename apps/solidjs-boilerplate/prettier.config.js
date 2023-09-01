850acb55379c018848057a3e8bc0d7798e82bcaf
sect <sect@sect.me>
2023-09-01 21:42:46 +0900

    chore(app): update dependencies

--- apps/solidjs-boilerplate/prettier.config.js
+++ apps/solidjs-boilerplate/prettier.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   // eslint-disable-next-line global-require, import/no-extraneous-dependencies
-  ...require('@spotify/prettier-config'),
-  // your overrides here
+  // ...require('@spotify/prettier-config'),
+  singleQuote: true,
+  trailingComma: 'all',
+  arrowParens: 'avoid',
 };
