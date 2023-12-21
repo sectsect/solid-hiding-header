7fde3a53f4fe551ec656da05f2bc40eee9e7cce4
sect <sect@sect.me>
2023-12-21 15:53:02 +0900

    test: avoid Playwright error 'missing dependencies' on Github Actions

--- apps/solidjs-boilerplate/playwright.config.ts
+++ apps/solidjs-boilerplate/playwright.config.ts
@@ -1,6 +1,6 @@
 import type { PlaywrightTestConfig } from '@playwright/test';
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { devices } from '@playwright/test';
+// import { devices } from '@playwright/test';
 
 /**
  * Read environment variables from file.
@@ -46,25 +46,31 @@ const config: PlaywrightTestConfig = {
   /* Configure projects for major browsers */
   projects: [
     {
-      name: 'chromium',
+      name: 'Google Chrome',
       use: {
-        ...devices['Desktop Chrome'],
-      },
+        channel: 'chrome',
+      }, // @ https://github.com/microsoft/playwright/issues/17975#issuecomment-1274884660
     },
+    // {
+    //   name: 'chromium',
+    //   use: {
+    //     ...devices['Desktop Chrome'],
+    //   },
+    // },
 
-    {
-      name: 'firefox',
-      use: {
-        ...devices['Desktop Firefox'],
-      },
-    },
+    // {
+    //   name: 'firefox',
+    //   use: {
+    //     ...devices['Desktop Firefox'],
+    //   },
+    // },
 
-    {
-      name: 'webkit',
-      use: {
-        ...devices['Desktop Safari'],
-      },
-    },
+    // {
+    //   name: 'webkit',
+    //   use: {
+    //     ...devices['Desktop Safari'],
+    //   },
+    // },
 
     /* Test against mobile viewports. */
     // {
