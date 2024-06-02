2631c413d67617c3071f47a3d38a00a4bcc00fda
sect <sect@sect.me>
2024-06-03 01:04:49 +0900

    test: added mocking api response to test

--- apps/solidjs-boilerplate/src/mocks/handlers.ts
+++ apps/solidjs-boilerplate/src/mocks/handlers.ts
@@ -1,14 +1,21 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { http, HttpResponse } from 'msw';
 
+import { posts } from '@/mocks/data/posts';
+
+const apiEndpointUrl = import.meta.env.VITE_PUBLIC_API_URL;
+
 export const handlers = [
   // Intercept "GET https://example.com/user" requests...
-  http.get('https://example.com/user', () => {
-    // ...and respond to them using this JSON response.
-    return HttpResponse.json({
-      id: 'c7b3d8e0-5e0b-4b0f-8b3a-3b9f4b3d3b3d',
-      firstName: 'John',
-      lastName: 'Maverick',
-    });
+  // http.get('https://example.com/user', () => {
+  //   // ...and respond to them using this JSON response.
+  //   return HttpResponse.json({
+  //     id: 'c7b3d8e0-5e0b-4b0f-8b3a-3b9f4b3d3b3d',
+  //     firstName: 'John',
+  //     lastName: 'Maverick',
+  //   });
+  // }),
+  http.get(`${apiEndpointUrl}/posts`, () => {
+    return HttpResponse.json(posts);
   }),
 ];
--- apps/solidjs-boilerplate/vitest.setup.ts
+++ apps/solidjs-boilerplate/vitest.setup.ts
@@ -2,7 +2,7 @@
 import '@testing-library/jest-dom/vitest';
 import { beforeAll, afterEach, afterAll } from 'vitest';
 
-import { server } from './src/mocks/server';
+import { server } from '@/mocks/server';
 
 // So this is basically saying any network requests that come through route them to mock service worker instead of the actual network.
 beforeAll(() => server.listen());
